2383f869201c808733398a798b91639aee0b5784
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2021-04-06 11:10:23 +1000

    prints the correct amount of stars. not spaces added yet

---
+++ Exercise6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 11; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    for (int j = i; j < 11; j++)
+                    {
+                        Console.Write("*");
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
