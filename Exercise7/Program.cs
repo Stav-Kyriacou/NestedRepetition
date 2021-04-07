9743c86ecb46766cffdf3c3f6576eec96c5e9f95
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2021-04-07 12:05:14 +1000

    create project initial commit

---
+++ Exercise7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reverse Pyramid Drawing");
+            Console.Write("How many lines should it be? ");
+
+            int rows = int.Parse(Console.ReadLine());               //ask user how many rows to draw
+            int stars = (rows * 2) - 1;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = i; j >= 1; j--)                        //draws the spaces
+                {
+                    Console.Write(" ");
+                }           
+
+                for (int k = 0; k < stars; k++)                     //draws the x's
+                {
+                    Console.Write("x");
+
+                }
+
+                Console.WriteLine();
+
+                stars -= 2;
+        }
+    }
+}
